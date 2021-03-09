77643913b3835c452483b425f3dca1d67cd571f5
namantyagi971 <naman.tyagi971@gmail.com>
2021-03-10 00:30:28 +0530

    First commit : create the directory structure and setup express server

---
+++ index.js
@@ -0,0 +1,18 @@
+// require the library
+const express = require('express');
+
+// fire up the server
+const app = express();
+
+// port to run the server
+const port = 8000;
+
+// checking whether server is running or not
+app.listen(port,function(err){
+     if(err)
+     {
+         console.log(`Error in running the server : ${err}`);
+         return;
+     }
+     console.log(`Server is up and running on port : ${port}`);
+});
