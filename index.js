a2cf408f838c3a65d474732f22963d7367f7a0a0
namantyagi971 <naman.tyagi971@gmail.com>
2021-03-10 20:19:55 +0530

    set up view engine : ejs

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ const app = express();
 // port to run the server
 const port = 8000;
 
+// set up view engine
+app.set('view engine','ejs');
+app.set('view','./views');
+
 // use express router
 app.use('/',require('./routes'));
 app.use('/users',require('./routes/users'));
