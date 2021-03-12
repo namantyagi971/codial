531ba04e1fc45eb8a5568c18e4aa5e758555133a
namantyagi971 <naman.tyagi971@gmail.com>
2021-03-12 20:09:29 +0530

    directory set up done

--- config/mongoose.js
+++ config/mongoose.js
@@ -9,3 +9,5 @@ db.on('error',console.error.bind("Oops!! Error in connecting to the databse"));
 db.once('open',function(){
     console.log("yeah!!! finally we have our MongoDB");
 });
+
+module.exports = db;
