f2c893d1b52db0603c8c217932ad9f55a0960ecf
namantyagi971 <naman.tyagi971@gmail.com>
2021-04-02 08:27:45 +0530

    checkauthentication on creating post

--- routes/posts.js
+++ routes/posts.js
@@ -1,9 +1,10 @@
 const express = require('express');
-
+const passport = require('passport');
 const router = express.Router();
 
 const postController = require('../controllers/post_controller');
 
-router.post('/create',postController.create);
+// passport.checkauthentication is used so that anonymous user can't post after creating dummy form in HTML
+router.post('/create',passport.checkAuthentication,postController.create);
 
 module.exports = router;
