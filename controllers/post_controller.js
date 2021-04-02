95af337c9ce1cfd213c03d143805e422d91c97da
namantyagi971 <naman.tyagi971@gmail.com>
2021-04-02 17:03:56 +0530

    delete post and comments

--- controllers/post_controller.js
+++ controllers/post_controller.js
@@ -1,5 +1,6 @@
 // importing the schema 
 const Post = require('../modals/post');
+const Comment = require('../modals/comment');
 
 // creating the post in database
 module.exports.create=function(req,res){
@@ -16,3 +17,29 @@ module.exports.create=function(req,res){
     });
 }
 
+// deleting the post from database
+module.exports.destroy = function(req,res){
+    //checking if the post exist or not because someone can fiddle my website through HTML inspect
+    console.log("delete id : ",req.query.id);
+    Post.findById(req.query.id,function(err,post){
+        if(err)
+        {
+            console.log("error in finding post from database",err);
+            return;
+        }
+        // if the current signed in user is same as the user who written the post
+        // .id converts the object id into string
+        if(req.user.id==post.user)
+        {
+            post.remove();
+            Comment.deleteMany({post:req.query.id},function(err){
+                return res.redirect('back');
+            });
+        }
+        else
+        {
+            return res.redirect('back');
+        }
+       
+    });
+}
--- routes/posts.js
+++ routes/posts.js
@@ -6,5 +6,6 @@ const postController = require('../controllers/post_controller');
 
 // passport.checkauthentication is used so that anonymous user can't post after creating dummy form in HTML
 router.post('/create',passport.checkAuthentication,postController.create);
+router.get('/delete/',passport.checkAuthentication,postController.destroy);
 
 module.exports = router;
