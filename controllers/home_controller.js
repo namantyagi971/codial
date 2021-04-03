cf3c8d40548ae5764a2a24334c3ba672c0565e0f
namantyagi971 <naman.tyagi971@gmail.com>
2021-04-03 16:47:36 +0530

    convert home action to async awiat

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -3,42 +3,67 @@ const Post = require('../modals/post');
 const User = require('../modals/user');
 
 // export to other files
-module.exports.home = function(req,res){
-    // printing the cookie coming from browser side
-    console.log(req.cookies);
-    // changing the cookie at server side
-    res.cookie('user_id',25);
-    // reading the post from database and display
-    // Post.find({},function(err,posts){
-    //     if(err)
-    //     {
-    //         console.log("error in finding post from database");
-    //         return;
-    //     }
-    //     return res.render('home',{
-    //         allposts : posts,
-    //         title : 'Codial | home'
-    //     });
-    // });
+// module.exports.home = function(req,res){
+//     // printing the cookie coming from browser side
+//     console.log(req.cookies);
+//     // changing the cookie at server side
+//     res.cookie('user_id',25);
+//     // reading the post from database and display
+//     // Post.find({},function(err,posts){
+//     //     if(err)
+//     //     {
+//     //         console.log("error in finding post from database");
+//     //         return;
+//     //     }
+//     //     return res.render('home',{
+//     //         allposts : posts,
+//     //         title : 'Codial | home'
+//     //     });
+//     // });
 
-    // populate the user of each post
-    Post.find({}).populate('user')
-    //  this is nested population
-    .populate({
-        path : 'comments',
-        populate : {
-            path : 'user'
-        }
-    })
-    .exec(function(err,posts){
+//     // populate the user of each post
+//     Post.find({}).populate('user')
+//     //  this is nested population
+//     .populate({
+//         path : 'comments',
+//         populate : {
+//             path : 'user'
+//         }
+//     })
+//     .exec(function(err,posts){
 
-        User.find({},function(err,users){
-            return res.render('home',{
+//         User.find({},function(err,users){
+//             return res.render('home',{
+//             allposts : posts,
+//             title : 'Codial | home',
+//             all_users : users
+//         });
+
+//     });
+// });
+// }
+
+// doing it async await way
+try{
+    module.exports.home = async function(req,res){
+
+       let posts = await Post.find({}).populate('user')
+            //  this is nested population
+            .populate({
+                path : 'comments',
+                populate : {
+                    path : 'user'
+                }
+            });
+
+        let users = await User.find({});
+        return res.render('home',{
             allposts : posts,
             title : 'Codial | home',
             all_users : users
         });
+    }
 
-    });
-});
+}catch{
+     console.log(`Error ${err}`);
 }
