735d7ae6ecb2cba71744a5101ec110d44511b7fb
namantyagi971 <naman.tyagi971@gmail.com>
2021-04-20 19:29:58 +0530

    set passport google-auth-2

---
+++ config/passport-google-oauth2-strategy.js
@@ -0,0 +1,48 @@
+const passport = require('passport');
+const googleStrategy = require('passport-google-oauth').OAuth2Strategy;
+
+const crypto = require('crypto');
+const User = require('../modals/user');
+
+// tell passport to use new strategy for google login
+ passport.use(new googleStrategy({
+        clientID : "193102948087-h08qf8u76of9m1dbslbv0eqi4vm99mc6.apps.googleusercontent.com",
+        clientSecret : "JugpAQ8iUvigRXgk_2WdcICw",
+        callbackURL : "http://localhost:8000/users/auth/google/callback"
+    },function(accessToken, refreshToken, profile, done){
+
+        // find a user
+        User.findOne({email:profile.emails[0].value}).exec(function(err,user){
+            if(err)
+            {
+                console.log("error in google-strategy passport",err); 
+                return;
+            }
+
+            console.log("***profile ",profile);
+
+            // if user found set this user as a req.user
+            if(user){
+                return done(null,user);
+            }
+
+            // if not found, create a user and set this user as a req.user
+            else{
+                User.create({
+                        name : profile.displayName,
+                        email : profile.emails[0].value,
+                        password : crypto.randomBytes(20).toString('hex')
+                    },function(err,user){
+                        if(err){
+                            console.log("error in creating user",err); 
+                            return;
+                        }
+                        else{
+                            return done(null,user);
+                        }
+                })
+            }
+        });
+    }));
+
+module.exports = passport;
